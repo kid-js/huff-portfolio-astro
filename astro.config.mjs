f0377f839b61fe8077efc3ddc60922498a80f8ac
kidjs <o-v-alm8@mail.ru>
2024-08-22 21:45:36 +0800

    changed build format and vite config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,17 @@ import cleanCss from 'vite-plugin-clean-css';
 
 // https://astro.build/config
 export default defineConfig({
+    build: {
+        format: 'file',
+    },
     output: 'static',
-    trailingSlash: 'never',
     prefetch: {
         prefetchAll: true,
     },
     vite: {
-        build: {
-            assetsInlineLimit: 2080,
-        },
+        // build: {
+        //     assetsInlineLimit: 2080,
+        // },
         plugins: [
             cleanCss({
                 level: 2,
