739ec227172bbc52a2844b290e1006ec581629bd
kidjs <o-v-alm8@mail.ru>
2024-08-27 18:10:20 +0800

    string replace plugin added to reduce bundle size

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 import cleanCss from 'vite-plugin-clean-css';
+import stringReplace from 'vite-plugin-string-replace';
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,6 +20,12 @@ export default defineConfig({
             cleanCss({
                 level: 2,
             }),
+            stringReplace([
+               {
+                  search: /astro-cid-[A-z0-9]{3}/g,
+                  replace: '',
+               },
+            ]),
         ],
     },
 });
